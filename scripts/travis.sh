b48456dcf81dfb63b5eb0c138e10da194307b737
Joe Hellerstein <jmh@berkeley.edu>
2018-07-08 16:31:27 -0700

    initial travis scripts

---
+++ scripts/travis.sh
@@ -0,0 +1,19 @@
+#! /usr/bin/env bash
+
+set -euo pipefail
+
+main() {
+    # Remove the build directory just in case there's something there already.
+    rm -rf build/"$TEST_TYPE";
+
+    # Build the code.
+    ./scripts/build.sh "$TEST_TYPE" 1;
+
+    # # Run all the unit tests.
+    # (cd build/"$TEST_TYPE" && ctest -L UNITTEST);
+
+    # # Run all the benchmarks.
+    # ./scripts/run_benchmarks.sh "$TEST_TYPE"
+}
+
+main
