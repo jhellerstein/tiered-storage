fc43edbd901797b908e42ec123b4b91ab3713674
Joe Hellerstein <jmh@berkeley.edu>
2018-07-08 19:17:59 -0700

    travis build protobuf from scratch

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -14,10 +14,22 @@ install_misc() {
       unzip
 }
 
+install_protobuf {
+  # check to see if protobuf folder is empty
+  if [ ! -d "$HOME/protobuf/lib" ]; then
+    wget https://protobuf.googlecode.com/files/protobuf-2.4.1.tar.gz
+    tar -xzvf protobuf-2.4.1.tar.gz
+    cd protobuf-2.4.1 && ./configure --prefix=$HOME/protobuf && make && make install
+  else
+    echo "Using cached directory."
+  fi
+}
+
 main() {
     set -x
     sudo apt-get -y update
     install_misc
+    install_protobuf
     set +x
 }
 
