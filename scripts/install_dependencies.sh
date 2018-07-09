e08fb11edc63b99b729eda0c6f001eb91da9436e
Joe Hellerstein <jmh@berkeley.edu>
2018-07-08 20:53:26 -0700

    build protobufs

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -14,22 +14,22 @@ install_misc() {
       unzip
 }
 
-# install_protobuf() {
-#   # check to see if protobuf folder is empty
-#   if [ ! -d "$HOME/protobuf/lib" ]; then
-#     wget https://github.com/google/protobuf/archive/v3.6.0.tar.gz
-#     tar -xzvf v3.6.0.tar.gz
-#     cd protobuf-3.6.0 && ./configure --prefix=$HOME/protobuf && make && make install
-#   else
-#     echo "Using cached directory."
-#   fi
-# }
+install_protobuf() {
+  # check to see if protobuf folder is empty
+  if [ ! -d "$HOME/protobuf/lib" ]; then
+    wget https://github.com/google/protobuf/archive/v3.6.0.tar.gz
+    tar -xzvf v3.6.0.tar.gz
+    cd protobuf-3.6.0 && ./configure --prefix=$HOME/protobuf && make && make install
+  else
+    echo "Using cached directory."
+  fi
+}
 
 main() {
     set -x
     sudo apt-get -y update
     install_misc
-    # install_protobuf
+    install_protobuf
     set +x
 }
 
