ac658f73a29a83cf16dda8b21a4d185cfcd47c86
Joe Hellerstein <jmh@berkeley.edu>
2018-07-08 17:26:42 -0700

    build protobuf in travis

---
+++ scripts/install_dependencies.sh
@@ -0,0 +1,36 @@
+#! /usr/bin/env bash
+
+set -euo pipefail
+
+install_misc() {
+    # These dependencies are needed by a couple of different projects including
+    # redis, protobuf, grpc, etc.
+    sudo apt-get install \
+      autoconf \
+      automake \
+      curl \
+      libtool \
+      make \
+      unzip
+}
+
+install_protobuf() {
+    git clone https://github.com/google/protobuf.git
+    cd protobuf/
+    ./autogen.sh
+    ./configure
+    make
+    make check
+    sudo make install
+    sudo ldconfig # refresh shared library cache.
+    protoc -h
+}
+
+main() {
+    set -x
+    sudo apt-get -y update
+    install_misc
+    set +x
+}
+
+main
